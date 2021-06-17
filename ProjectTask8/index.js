177d9da61836013fe021e0137da398488802d7d8
BATHULA BHARGAV REDDY <82729166+bhargavreddybathula@users.noreply.github.com>
2021-06-17 15:15:54 +0530

    Create index.js

---
+++ ProjectTask8/index.js
@@ -0,0 +1,38 @@
+var userName = '';
+
+'jane' ? console.log(`Hello, Jane!`):
+    console.log(`Hello!`);
+
+var userQuestion = 'Am I human being?'
+console.log(`Am i human being?`);
+const randomNumber = Math.floor(Math.random() * 8);
+
+switch (eightBall) {
+    case 0:
+        eightBall = 'It is certain'
+        break;
+    case 1:
+        eightBall = 'It is decidedly so'
+        break;
+    case 2:
+        eightBall = 'Reply hazy try again'
+        break;
+    case 3:
+        eightBall = 'Cannot predict now'
+        break;
+    case 4:
+        eightBall = 'Do not count on it'
+        break;
+    case 5:
+        eightBall = 'My sources say no'
+        break;
+    case 6:
+        eightBall = 'Outlook not so good'
+        break;
+    case 7:
+        eightBall = 'Signs point to yes'
+        break;
+}
+console.log(`the eight ball answer : 
+${eightBall}`)
+var eightBall = ''
