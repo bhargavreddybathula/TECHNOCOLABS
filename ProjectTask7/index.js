8ada74be7cde3d980ad6159325c27561c0f8759b
BATHULA BHARGAV REDDY <82729166+bhargavreddybathula@users.noreply.github.com>
2021-06-17 12:57:46 +0530

    Create index.js

---
+++ ProjectTask7/index.js
@@ -0,0 +1,20 @@
+//Initilizing myAge value to 20
+const myAge = 20;
+//Initilizing earlyYears1 value to 20
+const earlyYears1 = 2;
+//value of earlyYears is product of 10.5 and value of earlyYears1
+let earlyYears = earlyYears1 * 10.5;
+//laterYears value is difference of myAge and 2
+let laterYears = myAge - 2;
+//replacing laterYears value with the product of laterYears initial value and 4
+laterYears = laterYears * 4;
+//printing laterYears and elarlyYears values
+console.log(`${laterYears}and ${earlyYears}`);
+//Giving value of myAgeInDogYears as sum of laterYears and earlyYears
+let myAgeInDogYears = laterYears + earlyYears;
+//using string method text name has been given with value string of my name
+const name = 'BATHULA BHARGAV REDDY';
+//storing the lowercase of string valye of name in myName variable
+let myName = name.toLowerCase();
+//printing the statement that includes of myName;myAge;myAgeInDogYears using console.log
+console.log(`My name is ${myName} I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`)
